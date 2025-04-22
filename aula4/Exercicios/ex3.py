a92885719dfec9eb4bcef8106fcf8c9d7b9060b7
xXmarcosX <marcos.v3567@gmail.com>
2025-04-22 17:52:55 -0300

    Refatoração de códigos do exercício 3

--- aula4/Exercicios/ex3.py
+++ aula4/Exercicios/ex3.py
@@ -3,27 +3,26 @@ desconto = 0.0
 
 if compra >= 100 and compra < 200:
     print(f'Valor total da sua compra: {compra}')
+    print(f'Desconto: {compra * 0.05}')
 
     compra = compra - (compra * 0.05)
-    
-    print(f'Desconto: {compra * 0.05}')
+
     print(f'Desconto de 5% aplicado. Valor final da compra: {compra}')
 
 elif compra >= 200 and compra < 300:
     print(f'Valor total da sua compra: {compra}')
+    print(f'Desconto: {compra * 0.10}')
 
     compra = compra - (compra * 0.10)
-    
-    print(f'Desconto: {compra * 0.10}')
+
     print(f'Desconto de 10% aplicado. Valor final da compra: {compra}')
 
 elif compra >= 300:
-    print(f'Valor total da sua compra: {compra}')
+    print(f'Valor total da sua compra: {compra}')    
+    print(f'Desconto: {compra * 0.15}')
 
     compra = compra - (compra * 0.15)
-    
-    print(f'Desconto: {compra * 0.15}')
-    print(f'Valor total da sua compra: {compra}')
+
     print(f'Desconto de 15% aplicado. Valor final da compra: {compra}')
 
 else:
