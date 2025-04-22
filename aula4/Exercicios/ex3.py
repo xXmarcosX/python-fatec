93e68fabbca6ef34613b4043a48a8e0f08b98a93
xXmarcosX <marcos.v3567@gmail.com>
2025-04-22 17:51:02 -0300

    Resolução do exercício 3

---
+++ aula4/Exercicios/ex3.py
@@ -0,0 +1,31 @@
+compra = float(input('Digite o valor total da sua compra: '))
+desconto = 0.0
+
+if compra >= 100 and compra < 200:
+    print(f'Valor total da sua compra: {compra}')
+
+    compra = compra - (compra * 0.05)
+    
+    print(f'Desconto: {compra * 0.05}')
+    print(f'Desconto de 5% aplicado. Valor final da compra: {compra}')
+
+elif compra >= 200 and compra < 300:
+    print(f'Valor total da sua compra: {compra}')
+
+    compra = compra - (compra * 0.10)
+    
+    print(f'Desconto: {compra * 0.10}')
+    print(f'Desconto de 10% aplicado. Valor final da compra: {compra}')
+
+elif compra >= 300:
+    print(f'Valor total da sua compra: {compra}')
+
+    compra = compra - (compra * 0.15)
+    
+    print(f'Desconto: {compra * 0.15}')
+    print(f'Valor total da sua compra: {compra}')
+    print(f'Desconto de 15% aplicado. Valor final da compra: {compra}')
+
+else:
+    print(f'Valor total da sua compra: {compra}')
+    print('Você não gastou o suficiente para que o desconto seja aplicado')
